a01d18c811e19681cc4cdc267645a352ee55c268
gympohnpimol <gympohnpimol@gmail.com>
2020-11-22 23:03:12 +0900

    commit

---
+++ Time Series Forecasting/timeseries.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov 22 17:48:59 2020
+
+@author: gym
+"""
+# %%
+import pandas as pd
+train = pd.read_csv("Train.csv")
+test = pd.read_csv("Test.csv")
+#%%
